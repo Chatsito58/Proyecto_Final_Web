430e501e8b90c0f2116583c17147e4322c543d62
Chatsito GPT <chatsitogpt58@gmail.com>
2025-07-08 06:21:24 -0500

    full

--- SQLQuery_Actualizar_Canchas.sql
+++
@@ -1,7 +0,0 @@
--- Agregar la columna PrecioHora si no existe
-IF COL_LENGTH('Canchas','PrecioHora') IS NULL
-BEGIN
-    ALTER TABLE [dbo].[Canchas] ADD [PrecioHora] DECIMAL(10,2) NOT NULL DEFAULT 0;
-END
-GO
-
